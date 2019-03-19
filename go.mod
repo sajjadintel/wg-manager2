43208a853b75728babb77810f1c16931f8ca65ab
David Marby <david@mullvad.net>
2019-03-19 18:26:38 +0100

    Add wireguard integration tests

--- go.mod
+++ go.mod
@@ -2,9 +2,11 @@ module github.com/mullvad/wireguard-manager
 
 require (
 	github.com/DMarby/jitter v0.0.0-20190312004500-d77fd504dcfa
+	github.com/google/go-cmp v0.2.0
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jamiealquiza/envy v1.1.0
-	github.com/mdlayher/wireguardctrl v0.0.0-20190313224109-71fabb657240
+	github.com/mdlayher/wireguardctrl v0.0.0-20190318190647-8a114cef8e87
 	github.com/spf13/cobra v0.0.3 // indirect
 	github.com/spf13/pflag v1.0.3 // indirect
 )
+
---
+++ wireguard/wireguard_test.go
@@ -0,0 +1,137 @@
+package wireguard_test
+
+import (
+	"encoding/base64"
+	"net"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/mdlayher/wireguardctrl"
+	"github.com/mdlayher/wireguardctrl/wgtypes"
+	"github.com/mullvad/wireguard-manager/api"
+	"github.com/mullvad/wireguard-manager/wireguard"
+)
+
+// Integration tests for wireguard, not ran in short mode
+// Requires a wireguard interface named wg0 to be running on the system
+
+const testInterface = "wg0"
+
+var ipv4Net = net.ParseIP("10.99.0.0")
+var ipv6Net = net.ParseIP("fc00:bbbb:bbbb:bb01::")
+
+var apiFixture = api.WireguardPeerList{
+	api.WireguardPeer{
+		IPLeastsig: 1,
+		Ports:      []int{1234, 4321},
+		Pubkey:     base64.StdEncoding.EncodeToString([]byte(strings.Repeat("a", 32))),
+	},
+}
+
+var peerFixture = []wgtypes.Peer{{
+	PublicKey: wgKey(),
+	AllowedIPs: []net.IPNet{
+		net.IPNet{
+			IP:   net.ParseIP("10.99.0.1"),
+			Mask: net.CIDRMask(32, 32),
+		},
+		net.IPNet{
+			IP:   net.ParseIP("fc00:bbbb:bbbb:bb01::1"),
+			Mask: net.CIDRMask(128, 128),
+		},
+	},
+	ProtocolVersion: 1,
+}}
+
+func wgKey() wgtypes.Key {
+	key, _ := wgtypes.NewKey([]byte(strings.Repeat("a", 32)))
+	return key
+}
+
+func TestWireguard(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration tests")
+	}
+
+	client, err := wireguardctrl.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close()
+	defer resetDevice(t, client)
+
+	wg, err := wireguard.New([]string{testInterface}, ipv4Net, ipv6Net)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer wg.Close()
+
+	t.Run("add peers", func(t *testing.T) {
+		wg.UpdatePeers(apiFixture)
+
+		device, err := client.Device(testInterface)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if diff := cmp.Diff(peerFixture, device.Peers); diff != "" {
+			t.Fatalf("unexpected peers (-want +got):\n%s", diff)
+		}
+	})
+
+	t.Run("update peer ip", func(t *testing.T) {
+		apiFixture[0].IPLeastsig = 2
+		wg.UpdatePeers(apiFixture)
+
+		device, err := client.Device(testInterface)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		peerFixture[0].AllowedIPs[0].IP = net.ParseIP("10.99.0.2")
+		peerFixture[0].AllowedIPs[1].IP = net.ParseIP("fc00:bbbb:bbbb:bb01::2")
+
+		if diff := cmp.Diff(peerFixture, device.Peers); diff != "" {
+			t.Fatalf("unexpected peers (-want +got):\n%s", diff)
+		}
+	})
+
+	t.Run("remove peers", func(t *testing.T) {
+		wg.UpdatePeers(api.WireguardPeerList{})
+
+		device, err := client.Device(testInterface)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if diff := cmp.Diff([]wgtypes.Peer(nil), device.Peers); diff != "" {
+			t.Fatalf("unexpected peers (-want +got):\n%s", diff)
+		}
+	})
+}
+
+func resetDevice(t *testing.T, c *wireguardctrl.Client) {
+	t.Helper()
+
+	cfg := wgtypes.Config{
+		ReplacePeers: true,
+	}
+
+	if err := c.ConfigureDevice(testInterface, cfg); err != nil {
+		t.Fatalf("failed to reset%v", err)
+	}
+}
+
+func TestInvalidInterface(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration tests")
+	}
+
+	interfaceName := "nonexistant"
+
+	_, err := wireguard.New([]string{interfaceName}, net.ParseIP("127.0.0.1"), net.ParseIP("::1"))
+	if err == nil {
+		t.Fatal("no error")
+	}
+}
